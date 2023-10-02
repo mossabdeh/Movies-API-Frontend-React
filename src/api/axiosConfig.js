69f0cfdadc6e85e1205be9c194a027d543c22765
Mossab Deh <mossabdehane19@gmail.com>
2023-10-03 01:50:54 +0200

    modifying baseURL : in axiosConfig from localhost:8082: to the new render fenerated website

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-baseURL : 'http://localhost:8082/',
+baseURL : 'https://movies-api-dockerhub.onrender.com/',
 headers: {
     "Access-Control-Allow-Origin": "*", // Set the allowed origin(s) here or use a specific domain
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS", // Add allowed HTTP methods
